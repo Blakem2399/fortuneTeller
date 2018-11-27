1dcf7529fa48e763ebf7ef4cafd00569af550383
Blake M <Blake.manookin@gmail.com>
2018-11-27 08:48:42 -0700

    Added Javascript

---
+++ action.js
@@ -0,0 +1,27 @@
+let fortuneArray = [
+    ', A family member will dump gravy onto your head this christmas',
+    ', A friend will invite you to a Christmas party, and they will fail to mention its for Furries.',
+    ', A loved one will take you christmas shopping for 5 hours, and will not buy anything',
+    ', Its a Festivus Miracle!, Nothing bad happens!',
+    ', Your grandmother will be ran over by a reindeer, The broken hip leads to severe medical debt'
+];
+
+let nameArray =['billy', 'bob','joe','ross', 'twat'];
+let currentName = "";
+let currentFortune = "";
+
+function getFortune() {
+    let randomFortune = Math.floor((Math.random() * fortuneArray.length));
+    let randomName = Math.floor((Math.random() * nameArray.length));
+    setFortune(fortuneArray[randomFortune],nameArray[randomName]);
+
+
+
+}
+
+function setFortune(currentfortune,currentname) {
+    currentFortune = currentfortune;
+    currentName = currentname;
+    document.getElementById('output').innerHTML = currentname+' '+currentfortune;
+
+}
